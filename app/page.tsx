04e9cc28c015200012015d787bb43f4d7b210f62
Bailey Hamersly <baileyhamersly@Baileys-MBP-2.attlocal.net>
2025-07-12 11:15:07 -0400

    Initial home page and home layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,30 @@
-"use client";
+'use client'
+import { useState } from 'react'
+import './globals.css'
 
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
+export default function HomePage() {
+  const [query, setQuery] = useState<string>('')
+  const [search, setSearch] = useState<string>('trending')
 
-Amplify.configure(outputs);
-
-const client = generateClient<Schema>();
-
-export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    setSearch(query)
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
+    <main className="container">
+      <h1 className="title">Giphy Clone</h1>
+      <form onSubmit={handleSubmit} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for GIFs"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="search-input"
+        />
+        <div>The current search is : {search}
+        </div>
+      </form>
     </main>
-  );
+  )
 }
